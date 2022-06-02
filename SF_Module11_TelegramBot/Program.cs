75b896d36f5cf33c6b0fe205484ea797e53f7d0a
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-06-02 19:59:43 +0300

    Add project files.

---
+++ SF_Module11_TelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF_Module11_TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
